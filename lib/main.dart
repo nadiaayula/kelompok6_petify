476c40d2edaa9a600f4718ea50e559b8a9070afb
pankop <zakymuslim7@gmail.com>
2025-11-30 01:24:29 +0700

    feat: setup supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'features/virtual_pet_wellbeings/screens/vpm_home_screen.dart';
-import 'features/rewards/screens/rewards_page.dart';
-import 'features/virtual_pet_wellbeings/screens/dashboard_screen.dart';
 
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
-  runApp(MyApp());
+  await dotenv.load();
+  await Supabase.initialize(
+    url: 'https://cdfcsdwkpkkecssujmxk.supabase.co',
+    anonKey: const String.fromEnvironment('SUPABASE_ANON'),
+  );
+
+  print('Supabase client initialized: ${Supabase.instance.client}');
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
